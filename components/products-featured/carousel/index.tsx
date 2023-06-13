7af5b7b27a17b239e13a2c16a68c74c541242df2
FullStack Dev <whitehorse9021@gmail.com>
2023-06-13 00:54:30 -0500

    Carousel css fixes

--- components/products-featured/carousel/index.tsx
+++ components/products-featured/carousel/index.tsx
@@ -1,58 +1,70 @@
-import ProductItem from './../../product-item';
-import { ProductTypeList } from 'types';
+import ProductItem from "./../../product-item";
+import { ProductTypeList } from "types";
 
 // import Swiper core and required components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-let slidesPerView = 1.3;
-let centeredSlides = true;
-let spaceBetween = 30;
-if (process.browser) {
-  if(window.innerWidth > 768) {
-    slidesPerView = 3;
-    spaceBetween = 35;
-    centeredSlides = false;
-  }
-  if(window.innerWidth > 1024) {
-    slidesPerView = 4;
-    spaceBetween = 65;
-    centeredSlides = false;
-  }
-}
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useEffect, useState } from "react";
 
 type ProductsCarouselType = {
-  products: ProductTypeList[]
-}
+  products: ProductTypeList[];
+};
 
 const ProductsCarousel = ({ products }: ProductsCarouselType) => {
+  const [slidesPerView, setSlidesPerView] = useState(2);
+  const [centeredSlides, setCenteredSlides] = useState(false);
+  const [spaceBetween, setSpaceBetween] = useState(30);
+
+  useEffect(() => {
+    updateWindowSize();
+    window.addEventListener("resize", updateWindowSize);
+    return () => window.removeEventListener("resize", updateWindowSize);
+  }, []);
+
+  function updateWindowSize() {
+    if (window.innerWidth > 768) {
+      setSlidesPerView(3);
+      setSpaceBetween(35);
+      setCenteredSlides(false);
+    } else if (window.innerWidth > 1024) {
+      setSlidesPerView(4);
+      setSpaceBetween(65);
+      setCenteredSlides(false);
+    } else {
+      setSlidesPerView(2);
+      setSpaceBetween(30);
+      setCenteredSlides(false);
+    }
+  }
+
   if (!products) return <div>Loading</div>;
 
   return (
     <div className="products-carousel">
-      <Swiper 
-      spaceBetween={spaceBetween} 
-      loop={true} 
-      centeredSlides={centeredSlides} 
-      watchOverflow={true} 
-      slidesPerView={slidesPerView} 
-      className="swiper-wrapper">
-        {products.map(item => (
+      <Swiper
+        spaceBetween={spaceBetween}
+        loop={true}
+        centeredSlides={centeredSlides}
+        watchOverflow={true}
+        slidesPerView={slidesPerView}
+        className="swiper-wrapper"
+      >
+        {products.map((item) => (
           <SwiperSlide key={item.id}>
-            <ProductItem 
-              id={item.id} 
+            <ProductItem
+              id={item.id}
               name={item.name}
               price={item.price}
               color={item.color}
               discount={item.discount}
               currentPrice={item.currentPrice}
               key={item.id}
-              images={item.images} 
+              images={item.images}
             />
           </SwiperSlide>
         ))}
       </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default ProductsCarousel
+export default ProductsCarousel;
