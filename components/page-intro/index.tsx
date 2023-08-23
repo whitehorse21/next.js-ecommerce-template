a0ee5f900f6379abd46c48ed5080b11760b8c77f
FullStack Dev <whitehorse9021@gmail.com>
2023-08-23 01:44:16 -0400

    Improved user accessbility

--- components/page-intro/index.tsx
+++ components/page-intro/index.tsx
@@ -1,30 +1,39 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, {EffectFade, Navigation} from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { EffectFade, Navigation } from "swiper";
 
 SwiperCore.use([EffectFade, Navigation]);
 
 const PageIntro = () => {
-
   return (
-    <section className="page-intro">  
+    <section className="page-intro">
       <Swiper navigation effect="fade" className="swiper-wrapper">
         <SwiperSlide>
-          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/slide-1.jpg')" }}>
+          <div
+            className="page-intro__slide"
+            style={{ backgroundImage: "url('/images/slide-1.jpg')" }}
+          >
             <div className="container">
               <div className="page-intro__slide__content">
                 <h2>Sale of the summer collection</h2>
-                <a href="#" className="btn-shop"><i className="icon-right"></i>Shop now</a>
+                <a href="/products" className="btn-shop">
+                  <i className="icon-right"></i>Shop now
+                </a>
               </div>
             </div>
           </div>
         </SwiperSlide>
 
         <SwiperSlide>
-          <div className="page-intro__slide" style={{ backgroundImage: "url('/images/slide-2.jpg')" }}>
+          <div
+            className="page-intro__slide"
+            style={{ backgroundImage: "url('/images/slide-2.jpg')" }}
+          >
             <div className="container">
               <div className="page-intro__slide__content">
                 <h2>Make your house into a home</h2>
-                <a href="#" className="btn-shop"><i className="icon-right"></i>Shop now</a>
+                <a href="/products" className="btn-shop">
+                  <i className="icon-right"></i>Shop now
+                </a>
               </div>
             </div>
           </div>
@@ -41,7 +50,7 @@ const PageIntro = () => {
                 <p>On purchases over $199</p>
               </div>
             </li>
-            
+
             <li>
               <i className="icon-shipping"></i>
               <div className="data-item__content">
@@ -49,7 +58,7 @@ const PageIntro = () => {
                 <p>Our clients' opinions speak for themselves</p>
               </div>
             </li>
-            
+
             <li>
               <i className="icon-cash"></i>
               <div className="data-item__content">
@@ -61,7 +70,7 @@ const PageIntro = () => {
         </div>
       </div>
     </section>
-  )
+  );
 };
 
-export default PageIntro
+export default PageIntro;
