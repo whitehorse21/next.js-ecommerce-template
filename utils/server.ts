0bca298c6b73beaf2ca89dd0a061d7566a29875f
FullStack Dev <whitehorse9021@gmail.com>
2024-02-15 09:31:02 -0600

    Changed server base url

--- utils/server.ts
+++ utils/server.ts
@@ -1,3 +1,4 @@
-const dev = process.env.NODE_ENV !== "production";
+//const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "http://localhost:3000";
+//export const server = dev ? "http://localhost:3000" : "http://localhost:3000";
+export const server = "https://next-js-ecommerce-template-ten.vercel.app";
